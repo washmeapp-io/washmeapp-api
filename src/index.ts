3e832c2d057de44d2eec218f546dfdb10fc1ee0e
duribegl <110846568+duribegl@users.noreply.github.com>
2023-12-29 17:48:14 -0500

    Initial setup for lambda code

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export const handler = async (event: any): Promise<any> => {
+  const message = "Hello World!";
+  console.log(`Returning ${message}`);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(message),
+  };
+};
