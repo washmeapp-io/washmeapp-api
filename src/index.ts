4e1087168123a7310d17540fda94bd93bfc0839e
duribegl <110846568+duribegl@users.noreply.github.com>
2024-01-30 19:48:46 -0500

    Update hello world message

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export const handler = async (event: any): Promise<any> => {
-  const message = "Hello World!";
+  const message = "Hello World from washme app!";
   console.log(`Returning ${message}`);
   return {
     statusCode: 200,
