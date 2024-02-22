5de84e069934c768a01952f9dac47438602380d5
duribegl <110846568+duribegl@users.noreply.github.com>
2024-02-22 09:35:43 -0500

    Create user if does not exists

--- src/login/index.ts
+++ src/login/index.ts
@@ -1,13 +1,16 @@
 import {
+  AdminCreateUserCommand,
+  AdminCreateUserCommandInput,
   AuthFlowType,
   CognitoIdentityProviderClient,
   InitiateAuthCommand,
+  InitiateAuthCommandInput,
 } from "@aws-sdk/client-cognito-identity-provider";
 
 const client = new CognitoIdentityProviderClient({ region: "us-east-1" });
 
 export default async function (username: string) {
-  const signInParams = {
+  const signInParams: InitiateAuthCommandInput = {
     AuthFlow: AuthFlowType.CUSTOM_AUTH,
     ClientId: process.env.USER_POOL_CLIENT_ID,
     AuthParameters: {
@@ -22,6 +25,41 @@ export default async function (username: string) {
       body: JSON.stringify(response.AuthenticationResult),
     };
   } catch (error: any) {
+    if (error.message === "User does not exist.") {
+      try {
+        const createUserParams: AdminCreateUserCommandInput = {
+          UserPoolId: process.env.USER_POOL_ID, // Ensure this is set in your environment variables
+          Username: username,
+          UserAttributes: [
+            {
+              Name: "email",
+              Value: username,
+            },
+          ],
+          MessageAction: "SUPPRESS", // Use this to prevent sending a confirmation email immediately
+        };
+        await client.send(new AdminCreateUserCommand(createUserParams));
+        const signInParams: InitiateAuthCommandInput = {
+          AuthFlow: AuthFlowType.CUSTOM_AUTH,
+          ClientId: process.env.USER_POOL_CLIENT_ID,
+          AuthParameters: {
+            USERNAME: username,
+          },
+        };
+        const response = await client.send(
+          new InitiateAuthCommand(signInParams)
+        );
+        return {
+          statusCode: 200,
+          body: JSON.stringify(response.AuthenticationResult),
+        };
+      } catch (createError: any) {
+        return {
+          statusCode: 400,
+          body: JSON.stringify({ message: createError.message }),
+        };
+      }
+    }
     return {
       statusCode: 400,
       body: JSON.stringify({ message: error.message }),
